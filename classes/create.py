e1d4a7f5be754fb4702e575ec622cc7d9c370121
Jake Lourence Arañas Villar <79712099+Lourenceee@users.noreply.github.com>
2021-08-29 06:18:46 +0800

    Avoiding Duplicate Accounts

--- classes/create.py
+++ classes/create.py
@@ -11,9 +11,9 @@ class Create(QDialog):
     def __init__(self):
         super(Create, self).__init__()
         loadUi("ui/registerFrame.ui", self)
-        self.registerFrame_submitButton.clicked.connect(self.submitFunction)  
+        self.registerFrame_submitButton.clicked.connect(self.checkingDuplicateAccounts)  
     
-    def submitFunction(self):
+    def submitAccount(self):
         mycursor = mydb.cursor()
         sql = f"INSERT INTO users_accounts (db_username, db_password, db_email) VALUES ('{self.registerFrame_usernameLineEdit.text()}', '{self.registerFrame_passwordLineEdit.text()}', '{self.registerFrame_emailLineEdit.text()}')"      
         try:
@@ -25,3 +25,22 @@ class Create(QDialog):
         print("Data inserted")
         print(sql)
         mycursor.close()
+
+    def checkingDuplicateAccounts(self):
+        createmsg = QMessageBox()
+        mycursor = mydb.cursor()
+        sql = f"SELECT * from users_accounts WHERE db_username = '{self.registerFrame_usernameLineEdit.text()}' OR db_email = '{self.registerFrame_emailLineEdit.text()}'"
+        mycursor.execute(sql)
+        if mycursor.fetchall():
+            createmsg.setWindowTitle("Warning")
+            createmsg.setText("Username already exists")
+            createmsg.setIcon(QMessageBox.Critical)
+            x = createmsg.exec_()
+        else:
+            if self.registerFrame_confirmpasswordLineEdit.text() != self.registerFrame_passwordLineEdit.text():
+                createmsg.setWindowTitle("Warning")
+                createmsg.setText("Your password does not match")
+                createmsg.setIcon(QMessageBox.Critical)
+                x = createmsg.exec_()
+            else:
+                self.submitAccount()
